80ecbb3bbc5dc01368a89209fa5129638e0e7c66
SloEagle <jan.fegesh@gmail.com>
2023-03-01 19:13:55 +0100

    Updated Category model and data table and created the CRUD functions.

--- BlazorEcommerce_Prototype/Client/Services/CategoryService/CategoryService.cs
+++ BlazorEcommerce_Prototype/Client/Services/CategoryService/CategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using BlazorEcommerce_Prototype.Shared;
+using System.Net.Http.Json;
 
 namespace BlazorEcommerce_Prototype.Client.Services.CategoryService
 {
@@ -12,6 +13,42 @@ namespace BlazorEcommerce_Prototype.Client.Services.CategoryService
         }
 
         public List<Category> Categories { get; set; } = new List<Category>();
+        public List<Category> AdminCategories { get; set; }
+
+        public event Action OnChange;
+
+        public async Task AddCategory(Category category)
+        {
+            var response = await _http.PostAsJsonAsync("api/category/admin", category);
+            AdminCategories = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public Category CreateNewCategory()
+        {
+            var newCategory = new Category { isNew = true, Editing = true };
+            AdminCategories.Add(newCategory);
+            OnChange.Invoke();
+            return newCategory;
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var response = await _http.DeleteAsync($"api/category/admin/{categoryId}");
+            AdminCategories = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public async Task GetAdminCategories()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category/admin");
+            if (response != null && response.Data != null)
+            {
+                Categories = response.Data;
+            }
+        }
 
         public async Task GetCategories()
         {
@@ -20,6 +57,14 @@ namespace BlazorEcommerce_Prototype.Client.Services.CategoryService
             {
                 Categories = response.Data;
             }
+        }
+
+        public async Task UpdateCategory(Category category)
+        {
+            var response = await _http.PutAsJsonAsync("api/category/admin", category);
+            AdminCategories = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
         }
     }
 }
